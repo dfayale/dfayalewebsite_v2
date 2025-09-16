1218f50c520af36f3c1cd9daf5b791ee800fbf82
Jack C <j.carney@bulldogs.yale.edu>
2025-09-15 23:52:55 -0400

    upgrade react

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isCI = process.env.GITHUB_ACTIONS === 'true';
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
 
-export default nextConfig;
+export default {
+    output: 'export',
+    images: { unoptimized: true },
+    basePath: isCI ? `/${repo}` : '',
+    assetPrefix: isCI ? `/${repo}/` : '',
+};
