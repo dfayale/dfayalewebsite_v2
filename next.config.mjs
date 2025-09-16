2d05f4351bbc3fa6e05a974d6e5fc47654f98bc4
Jack C <j.carney@bulldogs.yale.edu>
2025-09-15 23:57:45 -0400

    fix next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isCI = process.env.GITHUB_ACTIONS === 'true';
-const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
+const nextConfig = {};
 
-export default {
-    output: 'export',
-    images: { unoptimized: true },
-    basePath: isCI ? `/${repo}` : '',
-    assetPrefix: isCI ? `/${repo}/` : '',
-};
+
+export default nextConfig;
